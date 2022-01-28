6e262f4d4a8d768554309031c25f8ab6d781b030
Alexander Rozilaynen <rozilaynen@gmail.com>
2022-01-28 16:48:21 +0300

    tests

--- spec/services/github/github_api_spec.rb
+++ spec/services/github/github_api_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Github::GithubApi do
   
   describe '#call' do
 	  before do 
-			stub_request(:get, "https://api.github.com/search/repositories?order=desc&page=10&q=ruby&sort=best%20match").to_return(
+			stub_request(:get, "https://api.github.com/search/repositories?order=desc&page=25&q=ruby&sort=best%20match").to_return(
 				status: 200, 
 				body: File.read('spec/fixtures/response.json'), 
 				headers: {})
@@ -15,11 +15,11 @@ RSpec.describe Github::GithubApi do
 		end
 	
 		it 'should return success request with page 10' do
-			response = described_class.call('ruby', 10)
+			response = described_class.call('ruby', 25)
 
 			expect(response['total_count']).to be(365_132)
 			expect(response['incomplete_results']).to be_falsey
-			# expect(response['items'].first['full_name']).to eq ("ruby/ruby")
+			expect(response['items'].first['full_name']).to eq ("ua-parser/uap-ruby")
 		end
 
 		it 'should return page 1 with params page 35' do 
